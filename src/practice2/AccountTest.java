c5feccb1f4cb3e412e5a6fe8512602d383f2c526
lllverb <54735495+lllverb@users.noreply.github.com>
2020-03-14 16:21:13 +0900

    わすれもの

---
+++ src/practice2/AccountTest.java
@@ -0,0 +1,24 @@
+package practice2;
+
+public class AccountTest {
+
+	public static void main(String[] args) {
+		testInstantiate();
+		testTransfer();
+	}
+	private static void testInstantiate() {
+		System.out.println("newできるかテスト");
+		Account a = new Account("ミナト", 30000);
+		if(!"ミナト".equals(a.owner)) {
+			System.out.println("失敗!名義人がおかしい");
+		}
+		if(30000 != a.balance) {
+			System.out.println("失敗!残高がおかしい");
+		}
+		System.out.println("テストを終了します");
+	}
+	private static void testTransfer() {
+
+	}
+
+}
